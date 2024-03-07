9889fa32e1c4ff81b0585b5f758ba18f84021a9c
andy4l <andyramos84@gmail.com>
2024-03-07 11:42:04 -0600

    Added store page that was preventing build.

---
+++ app/store/page.js
@@ -0,0 +1,17 @@
+// pages/WIP.js
+//"use client";
+import React from "react";
+
+const store = () => {
+  return (
+    <div>
+      <h1>Webstore in Progress</h1>
+      <p>
+        We&apos;re working hard to get this site ready for you. Please check
+        back soon!
+      </p>
+    </div>
+  );
+};
+
+export default store;
